281f736baa0698cd2254dd3b2f3847e3aa1cbbd2
Magda7Lena <magdalena7pawlak@gmail.com>
2021-02-09 19:35:25 +0100

    init board first steps

--- game.js
+++ game.js
@@ -1,7 +1,52 @@
-initGame();
 
-function initGame() {
+let symbols = [
+    {name: "donut", img:"img/donut.png",},
+    {name: "donut", img:"img/donut.png",},
+    {name: "donut", img:"img/donut.png",},
+    {name: "donut", img:"img/donut.png",},
+    {name: "flamingo", img:"img/flamingo.png",},
+    {name: "flamingo", img:"img/flamingo.png",},
+    {name: "flamingo", img:"img/flamingo.png",},
+    {name: "flamingo", img:"img/flamingo.png",},
+    {name: "cactus", img:"img/cactus.png",},
+    {name: "cactus", img:"img/cactus.png",},
+    {name: "cactus", img:"img/cactus.png",},
+    {name: "cactus", img:"img/cactus.png",},
+    {name: "emot", img:"img/emot.png",},
+    {name: "emot", img:"img/emot.png",},
+    {name: "emot", img:"img/emot.png",},
+    {name: "emot", img:"img/emot.png",},
+]
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
+let grid = document.querySelectorAll('slot')
+
+
+
+
+function initBoard() {
+    let slot = document.createElement('div')
+    slot.setAttribute('slot')
+    let div = document.querySelector('.board')
+    div.appendChild(slot)
+
+
+    let img = document.createElement("img");
+    img.setAttribute('src','img/donut.png');
+    let parent = document.querySelector(".symbols");
+    parent.appendChild(img)
+
+
+    // for(let symbol of symbols){
+    //     let img = document.createElement('img')
+    //     img.setAttribute('src','img/donut.png')
+    // }
 
 }
+
+
+function initGame() {
+    initBoard()
+
+}
+
+initGame();
